c7ca453bf8f1ff73861d33c64ac803b7195ddfe6
notchwis <kodenothing@gmail.com>
2026-05-29 14:09:05 -0500

    Update visualizer.js

--- js/visualizer.js
+++ js/visualizer.js
@@ -15,20 +15,20 @@ function drawWatermark(ctx, W, H) {
   ctx.shadowBlur = 2;
   ctx.shadowColor = 'black';
   ctx.textAlign = 'left';
+  ctx.textBaseline = 'top';
   
-  const padding = 12;
-  const topPadding = 16;
+  const padding = 8;
   const lineHeight = baseSize + 3;
   
   // Emoji on left
-  ctx.fillText('🐱', padding, topPadding + baseSize);
+  ctx.fillText('🐱', padding, padding);
   
   // Text next to emoji (shorter version)
-  ctx.fillText('NekoBeats', padding + 22, topPadding + baseSize);
+  ctx.fillText('NekoBeats', padding + 22, padding);
   
   // URL below (shorter)
   ctx.font = `${Math.max(9, baseSize - 2)}px monospace`;
-  ctx.fillText('sides.catsdevs.online/NekoBeats-Web', padding + 22, topPadding + baseSize + lineHeight);
+  ctx.fillText('sides.catsdevs.online/NekoBeats-Web', padding + 22, padding + lineHeight);
   
   ctx.restore();
 }
